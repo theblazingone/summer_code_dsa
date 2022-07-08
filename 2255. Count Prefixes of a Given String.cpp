db7d03dc65e77b66572169f0975c6632ad0f8c01
theblazingone <75668411+theblazingone@users.noreply.github.com>
2022-07-08 18:49:01 +0530

    Add files via upload

---
+++ 2255. Count Prefixes of a Given String.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countPrefixes(vector<string>& words, string s) {
+        
+        int cnt = 0;
+        
+        for (int i = 0; i < words.size(); i++){
+            
+            if (s.find(words[i]) == 0){
+                cnt++;
+            }
+        }
+        
+        return cnt;
+    }
+};
