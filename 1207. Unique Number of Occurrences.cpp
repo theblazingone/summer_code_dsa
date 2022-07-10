0cf430a3e7cc1978ab1f1291c46e6d32521ec70a
theblazingone <75668411+theblazingone@users.noreply.github.com>
2022-07-10 19:11:56 +0530

    Add files via upload

---
+++ 1207. Unique Number of Occurrences.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        
+        unordered_map <int, int> mp;
+        for (int i = 0; i < arr.size(); i++){
+            mp[arr[i]]++;
+        }
+        
+        unordered_set <int> s;
+        for  (auto it: mp){
+            
+            if (s.find(it.second) != s.end()){
+                return false;
+            }
+            s.insert(it.second);
+        }
+        
+        return true;
+    }
+};
